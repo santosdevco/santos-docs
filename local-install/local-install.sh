da62d9261b188145b1cf0199baf73e572a0f8f1e
Santiago Andres Ramirez Chavez <rami802288@siigo.com>
2022-06-23 15:20:44 -0500

    feature: open server url after install  it was added

--- local-install/local-install.sh
+++ local-install/local-install.sh
@@ -9,7 +9,7 @@ servicename=${servicename:-$DEFAULT_SERVICENAME}
 port=${port:-$DEFAULT_PORT}
 servicetemplatepath=mydocs.service
 servicefinalpath="/etc/systemd/system/$servicename.service"
-
+ulr=http://localhost:$port
 echo "documentation project source will be in $vpath"
 echo "service name will be  $servicename"
 echo "server port will be  $port"
@@ -17,7 +17,7 @@ read -p "ENTER to continue, else press clt C" vcontinue
 
 echo "downloading project in $vpath"
 git clone https://github.com/SantiagoAndre/SRE-Meetings-Directory/  $vpath 
-sudo rm -r $vpath/.git/
+# sudo rm -r $vpath/.git/
 echo -e "PORT=$port\nSERVICENAME=$servicename" >> $vpath/.env
 echo "temp downloading service template in $servicetemplatepath"
 wget https://raw.githubusercontent.com/SantiagoAndre/SRE-Meetings-Directory/local-install/local-install/mydocs.service -O $servicetemplatepath
@@ -33,7 +33,14 @@ sudo systemctl start "$servicename.service"
 echo "enabling service $servicename"
 sudo systemctl enable "$servicename.service"
 echo "documentation server installed"
-echo "open http://localhost:$port to see your new documentation server"
+if which xdg-open > /dev/null
+then
+  xdg-open url
+elif which gnome-open > /dev/null
+then
+  gnome-open url 
+fi
+echo "open $url to see your new documentation server"
 echo "run the next commands to interact with your service $servicename"
 echo "sudo systemctl status $servicename.service"
 echo "sudo systemctl start $servicename.service"
